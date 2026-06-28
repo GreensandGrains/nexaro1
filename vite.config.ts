fb16738edcecb91a5b0ab5390d05265a4d508e08
GreensandGrains <greensandgrains2025@gmail.com>
2026-06-28 13:14:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
   server: {
     host: "0.0.0.0",
     port: 8080,
-    allowedHosts: ["nexaro.up.railway.app"],
+    allowedHosts: ["nexaro.up.railway.app", "nexaro1-1.onrender.com"],
   },
 });
